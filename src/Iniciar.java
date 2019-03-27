7ab1e3e2c5b1b4319e432741c96bbb1f582c7ff1
Sarah <sarah.lacerda@mail.com>
2019-03-27 17:02:28 -0300

    Correcting failures

--- src/Iniciar.java
+++ src/Iniciar.java
@@ -1,82 +1,66 @@
-import com.sun.deploy.util.SystemUtils;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
-
 import javafx.scene.image.Image;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.net.InetAddress;
 
 public class Iniciar extends Application {
     static String ip = null;
+    static File peraVereira;
 
     @Override
     public void start(Stage primaryStage) {
-        /* Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-         */
-        File f = new File("C:\\Users\\lacep\\IdeaProjects\\Pera\\src\\rindo.png");
-        Image image = new Image(f.toURI().toString());
+
+        Image image = new Image(peraVereira.toURI().toString());
         ImageView imageView = new ImageView(image);
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setGraphic(imageView);
         alert.setTitle("Sistema Iniciado");
-        alert.setHeaderText("CONECTE-SE COM: " + ip + "    ");
-        alert.setContentText("Sistema iniciado com sucesso! Digite o endereco acima para conectar \nAVISO: Nao abra essa janela multiplas vezes.\nMANTENHA ESSA JANELA ABERTA ATE O FINAL DA APLICACAO DOS JOGOS\nA TRANSMISSAO SERA PARADA CASO ELA VENHA SER FECHADA\nPARA TERMINAR A EXECUCAO APERTE O BOTAO OK ABAIXO");
-        AnchorPane root=new AnchorPane();
+        if (ip.contains("127.0.0.1") || ip.contains("0.0.0.0") || ip == null) {
+            alert.setHeaderText("O sistema foi iniciado mas nao foi possivel detectar o endereco IP do computador atual\nVerifique o mesmo nas configuracoes do computador e use-o para acessar as atividades");
+        } else {
+            alert.setHeaderText("CONECTE-SE COM: " + ip + "    ");
+        }
+        alert.setContentText("Sistema iniciado com sucesso! Digite o endereco acima nos navegadores onde as atividades serao aplicadas \nAVISO: NAO ABRA ESSA JANELA MULTIPLAS VEZES.\nMANTENHA ESSA JANELA ABERTA ATE O FINAL DA APLICACAO DOS JOGOS\nA TRANSMISSAO SERA PARADA AO FECHAR\nPARA TERMINAR A EXECUCAO APERTE O BOTAO OK ABAIXO");
         alert.showAndWait();
-
-//        StackPane root = new StackPane();
-        //root.getChildren().add(btn);
 
 
     }
+
     public static void main(String[] args) {
         Process process = null;
         String os = System.getProperty("os.name").toLowerCase();
-        if(os.contains("win")) {
+        if (os.contains("win")) {
             try {
+                peraVereira = new File(System.getProperty("user.dir") + "\\DATA\\pera.png");
                 InetAddress localhost = InetAddress.getLocalHost();
                 ip = (localhost.getHostAddress()).trim();
                 String path = System.getProperty("user.dir") + "\\DATA";
                 //path = path.replace("src", "DATA");
                 process = new ProcessBuilder(path + "\\WinServer.exe").start();
+                launch(args);
+                process.destroy();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                peraVereira = new File(System.getProperty("user.dir") + "/DATA/pera.png");
+                InetAddress localhost = InetAddress.getLocalHost();
+                ip = (localhost.getHostAddress()).trim();
+                String path = System.getProperty("user.dir") + "/DATA";
+                ProcessBuilder pythonHttpServer = new ProcessBuilder();
+                pythonHttpServer.command("bash", "-c", "python3 -m http.server");
+                launch(args);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        else if(os.contains("osx")){
-            try {
-            InetAddress localhost = InetAddress.getLocalHost();
-            ip = (localhost.getHostAddress()).trim();
-            String path = System.getProperty("user.dir") + "\\DATA";
-            //path = path.replace("src", "DATA");
-            process = new ProcessBuilder(path + "\\OSXServer.dmg").start();
-            } catch (Exception e) {
-            e.printStackTrace();
-        }}
-        launch(args);
-        process.destroy();
     }
 
 }
